a486b04633c4849c562fb3ec099babec6da7f1e9
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2022-05-07 13:05:42 -0400

    adding files

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	s3Bucket := os.Getenv("S3_BUCKET")
+	secretKey := os.Getenv("SECRET_KEY")
+
+	// now do something with s3 or whatever
+	println(s3Bucket, secretKey)
+}
